77a092bcce8688bf61b71eb803b380b65d82a40f
41223140 <144767668+41223140@users.noreply.github.com>
2024-01-16 07:54:48 +0000

    新增ANSIC標題

---
+++ downloads/C1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 黃耀韋\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : May 29,2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 09-66300055\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
